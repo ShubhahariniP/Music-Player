be9173a933bc50c3c062728fcfaaad455692a3b6
ShubhahariniP <155978749+ShubhahariniP@users.noreply.github.com>
2024-11-11 18:39:50 +0530

    Add files via upload

---
+++ MUSIC PLAYER/js/playlist.js
@@ -0,0 +1,36 @@
+let song=[
+    {
+        name:"Hey minnale",
+        artist:"GV prakash,Hariharan and Shewta Mohan",
+        img:"img1",
+        audio:"music1"
+
+},
+{
+    name:"Kanmani anbodu",
+    artist:"Kamal Haasan and S.Janki",
+    img:"img2",
+    audio:"music2"
+
+},
+{
+    name:"beast",
+    artist:"Anirudha",
+    img:"img3",
+    audio:"music3"
+
+},
+{
+    name:"Vaidehi-Kathirunthal",
+    artist:"llairaja",
+    img:"img4",
+    audio:"music4"
+
+},
+{
+name:"Snehithane",
+artist:"A.R. Rahman,Sadhana Sargam, Rotimi",
+img:"img5",
+audio:"music5"
+
+},]
